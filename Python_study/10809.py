7a9fff06af61d450dd6dbb8251d81799f75a9a64
SJYUN00 <pender0207@gmail.com>
2022-04-11 04:56:00 +0900

    dspro

--- Python_study/10809.py
+++ Python_study/10809.py
@@ -1,6 +1,6 @@
-S = input()
+# S = input()
 
-S_char = list(map(str,S))
+# S_char = list(map(str,S))
 
 
 # 알파벳 리스트 인덱싱 리스트 X로
@@ -14,19 +14,20 @@ S_char = list(map(str,S))
 
 # 입력받은 문자열 길이
 
-strlen = len(S)
+# strlen = len(S)
 # print(strlen)
 
 # 문자열 인덱싱
-for i in S:
-  for j in range(i+1, strlen+1):
-    if i != strlen-1:
-      if S_char[0][i] == S_char[0][j]:
-       S_char[0][j] == 'A'
-       print(S_char)
-    elif i == strlen-1:
-      if S_char[0][i] == 'A':
-        break
+
+# for i in S:
+#   for j in range(i+1, strlen+1):
+#     if i != strlen-1:
+#       if S_char[0][i] == S_char[0][j]:
+#        S_char[0][j] == 'A'
+#        print(S_char)
+#     elif i == strlen-1:
+#       if S_char[0][i] == 'A':
+#         break
 
 
 
@@ -42,3 +43,8 @@ for i in S:
 # ['a', 'd', 'c', , , 'e']
 
 
+# Find 함수 이용
+word = input()
+
+for i in range(97,123):
+  print(word.find(chr(i)))
